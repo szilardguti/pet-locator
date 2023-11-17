53b4144adac797d7a7b3f862cd4e960be3f3c74b
szilardguti <szilardguti@gmail.com>
2023-11-17 18:00:55 +0100

    Add cat and dog POST/GET APIs

--- backend/services/server.js
+++ backend/services/server.js
@@ -1,13 +1,76 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+var fs = require("fs");
 
 const app = express();
+const dogFilePath = "../resources/dogs.geojson";
+const catFilePath = "../resources/cats.geojson";
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.get("/api/test", (req, res) => {
-  res.json({ hello: "world!" });
+app.post("/api/dogs", (req, res) => {
+  const jsonData = req.body;
+
+  fs.writeFile(dogFilePath, JSON.stringify(jsonData), (err) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send("Internal Server Error");
+    } else {
+      console.log("Data saved to dogs.geojson");
+      res.status(200).send("OK");
+    }
+  });
+});
+
+app.get("/api/dogs", (req, res) => {
+  fs.readFile(dogFilePath, "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send("Internal Server Error");
+    } else {
+      try {
+        const jsonData = JSON.parse(data);
+        console.log("Data returned from dogs.geojson");
+        res.status(200).json(jsonData);
+      } catch (jsonError) {
+        console.error(jsonError);
+        res.status(500).send("Error parsing GeoJSON file");
+      }
+    }
+  });
+});
+
+app.post("/api/cats", (req, res) => {
+  const jsonData = req.body;
+
+  fs.writeFile(catFilePath, JSON.stringify(jsonData), (err) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send("Internal Server Error");
+    } else {
+      console.log("Data saved to cats.geojson");
+      res.status(200).send("OK");
+    }
+  });
+});
+
+app.get("/api/cats", (req, res) => {
+  fs.readFile(catFilePath, "utf8", (err, data) => {
+    if (err) {
+      console.error(err);
+      res.status(500).send("Internal Server Error");
+    } else {
+      try {
+        const jsonData = JSON.parse(data);
+        console.log("Data returned from cats.geojson");
+        res.status(200).json(jsonData);
+      } catch (jsonError) {
+        console.error(jsonError);
+        res.status(500).send("Error parsing GeoJSON file");
+      }
+    }
+  });
 });
 
 const port = 3000;
